78dd811b91ebfccfeed5a448824d48d1032398df
Ander <volintine_21001524@utp.edu.my>
2025-09-21 17:30:13 +0800

    pwm poke around source code (tested on 6.12.28-rt9-v8_full_preempt)

---
+++ anc/docs/guide.md
@@ -0,0 +1,6 @@
+## Half-ass deployment
+
+> can't be arsed to deal with CI/CD, best I can do is copy pasting scp commands atm
+
+Deploy ANC to Pi:
+scp -i ~/gipop_plc /home/ander/Documents/lagadan/repo/torunggari/anc/target/aarch64-unknown-linux-gnu/release/anc pi@172.30.40.32:/home/pi/torunggari/anc/
--- anc/src/main.rs
+++ anc/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use std::time::Duration;
+use rpi_pal::pwm::{Channel, Polarity, Pwm};
+
+const PERIOD_NS: u64 = 1_000_000;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let pwm = Pwm::with_pwmchip(0, Channel::Pwm1 as u8)?;
+    pwm.set_period(Duration::from_nanos(PERIOD_NS))?;
+    pwm.set_polarity(Polarity::Normal)?;
+    pwm.enable()?;
+
+    for _ in 1..=12 {
+        pwm.set_pulse_width(Duration::from_nanos(PERIOD_NS / 8))?;
+        std::thread::sleep(Duration::from_millis(300));        
+        pwm.set_pulse_width(Duration::from_nanos(PERIOD_NS / 4))?;
+        std::thread::sleep(Duration::from_millis(300));
+        pwm.set_pulse_width(Duration::from_nanos(PERIOD_NS / 2))?;
+        std::thread::sleep(Duration::from_millis(300));
+        pwm.set_pulse_width(Duration::from_nanos(PERIOD_NS))?;
+        std::thread::sleep(Duration::from_millis(300));
+    }
+
+    Ok(())
+    // When the pwm variable goes out of scope, the PWM channel is automatically disabled.
+    // You can manually disable the channel by calling the Pwm::disable() method.
 }
