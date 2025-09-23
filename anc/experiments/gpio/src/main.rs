f29a78ff7a1215ea98df60f432d039f2c5abe087
Ander <volintine_21001524@utp.edu.my>
2025-09-23 16:17:51 +0800

    should work, havent tested

--- anc/experiments/gpio/src/main.rs
+++ anc/experiments/gpio/src/main.rs
@@ -2,7 +2,7 @@ use gpio_cdev::{Chip, Error, LineHandle, LineRequestFlags};
 use libc::*;
 use env_logger::Env;
 use core_affinity::*;
-use std::time::*;
+use std::{ops::Mul, time::*};
 
 /* Minimally viable code to demonstrate HC-SR04 on the Pi 5 */
 
@@ -18,6 +18,43 @@ pub enum EchoState {
     Transmitted,
     Reflected,
 }
+
+pub enum DistanceUnit {
+    mm(f64),
+    cm(f64),
+}
+
+impl DistanceUnit {
+    pub fn write_val(&mut self, new_val: f64) {
+        match self {
+            DistanceUnit::mm(val) => *val = new_val,
+            DistanceUnit::cm(val) => *val = new_val,
+        }
+    }
+
+    pub fn to_val(&self) -> f64 {
+        match self {
+            DistanceUnit::mm(val) => *val,
+            DistanceUnit::cm(val) => *val,
+        }
+    }
+}
+
+pub enum VelocityUnit {
+    m_per_s(f64),
+    cm_per_ms(f64),
+}
+
+impl VelocityUnit {
+    pub fn to_val(&self) -> f64 {
+        match self {
+            VelocityUnit::m_per_s(val) => *val,
+            VelocityUnit::cm_per_ms(val) => *val,
+        }
+    }
+}
+
+const SPEED_OF_SOUND: VelocityUnit = VelocityUnit::m_per_s(343.0);
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
@@ -65,6 +102,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut trig_state: TrigState = TrigState::NotTriggered;
     let mut echo_state: EchoState = EchoState::NotTransmitted;
     let mut echo_transmit_timestamp: Instant = Instant::now(); // Need to add assertion that in steady-state this cannot be less than the next cycle time
+    let mut echo_reflect_timestamp: Instant = Instant::now();
+    let mut time_of_flight: Duration = Duration::from_millis(0);
+    let mut distance: DistanceUnit = DistanceUnit::cm(0.0);
     loop {
         // Check echo first, then trigger
         let echo = get_echo(&echo_hndl)?;
@@ -77,13 +117,22 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         
         // Transmitted: Echo HIGH after Trig triggered
         if echo == 1 && trig_state == TrigState::Triggered {
+            echo_transmit_timestamp = Instant::now();
             echo_state = EchoState::Transmitted;
         }
 
         // Reflected: Echo LOW after Trig triggered
         if echo == 0 && trig_state == TrigState::Triggered {
+            echo_reflect_timestamp = Instant::now();
+            time_of_flight = echo_reflect_timestamp - echo_transmit_timestamp;
             echo_state = EchoState::Reflected;
         }
+
+        let dist = (time_of_flight.as_secs() as f64 * SPEED_OF_SOUND.to_val()) / 2.0 ;
+        distance.write_val(dist);
+
+        log::info!("ToF: {}", time_of_flight.as_millis());
+        log::info!("Distance: {}", distance.to_val());
 
         counter = counter.wrapping_add(1);
         next_time += cycle;
