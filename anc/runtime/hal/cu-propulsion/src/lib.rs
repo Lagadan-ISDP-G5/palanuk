9a92b8b538946613afb7e94cc43c95af73c04087
andergisomon <volintine_21001524@utp.edu.my>
2025-11-01 22:58:27 +0800

    propulsion payload

--- anc/runtime/hal/cu-propulsion/src/lib.rs
+++ anc/runtime/hal/cu-propulsion/src/lib.rs
@@ -1,10 +1,21 @@
-use dumb_sysfs_pwm::*;
-
+use dumb_sysfs_pwm::{Pwm, PwmChip};
 use cu29::prelude::*;
 use bincode::{Decode, Encode};
 use serde::{Deserialize, Serialize};
 
+#[derive(Debug, Clone, Copy, Default, Encode, Decode, PartialEq, Serialize, Deserialize)]
+pub struct PropulsionPayload {
+    left_enable: bool,
+    right_enable: bool,
+    left_speed: f64,
+    right_speed: f64,
+    left_direction: WheelDirection,
+    right_direction: WheelDirection
+}
+
+#[derive(Debug, Clone, Copy, Default, PartialEq, Encode, Decode, Serialize, Deserialize)]
 pub enum WheelDirection {
+    #[default]
     Forward,
     Reverse
 }
@@ -20,8 +31,19 @@ pub struct Propulsion {
     right_wheel: Wheel
 }
 
-impl CuSinkTask for Propulsion {
+impl Freezable for Propulsion {}
 
+impl CuSinkTask for Propulsion {
+    type Input<'m> = input_msg!(PropulsionPayload)
+    fn new(_config: Option<&ComponentConfig>) -> Result<Self, CuError>
+        where Self: Sized
+    {
+        Ok(Self { left_wheel: (), right_wheel: () })
+    }
+
+    fn process(&mut self, _clock: &RobotClock, input: &Self::Input<'_>) -> Result<(), CuError> {
+        Ok(())
+    }
 }
 
 #[cfg(test)]
