1a33c7684b6d5dd794cd72982c24dd75562e9234
andergisomon <volintine_21001524@utp.edu.my>
2025-11-15 23:37:28 +0800

    finish impl power monitor task (cu-powermon)

--- anc/runtime/hal/cu-powermon/src/lib.rs
+++ anc/runtime/hal/cu-powermon/src/lib.rs
@@ -1,16 +1,76 @@
-use dumb_ina219::*;
+use dumb_ina219::{units::{CurrentUnit, Gettable, ResistanceUnit}, *};
+use cu29::prelude::*;
+use bincode::{Decode, Encode};
+use serde::{Deserialize, Serialize};
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub struct CuIna219 {
+    driver_instance: Ina219,
+    target_addr: u8,
+}
+
+#[derive(Debug, Clone, Copy, Encode, Decode, Default, PartialEq, Serialize, Deserialize)]
+pub struct Ina219Payload {
+    pub power: f64,
+    pub load_current: f64,
+    pub shunt_voltage: f64,
+    pub bus_voltage: f64,
+    pub target_addr: u8,
+}
+
+// Sensor is stateless
+impl Freezable for CuIna219 {}
+
+impl CuSrcTask for CuIna219 {
+    type Output<'m> = output_msg!(Ina219Payload);
+    fn new(config: Option<&ComponentConfig>) -> CuResult<Self>
+    where Self: Sized
+    {
+        let ComponentConfig(kv) =
+            config.ok_or("No ComponentConfig specified for GPIO in RON")?;
+
+        let target_addr: u8 = kv
+            .get("target_addr")
+            .expect("I2C target address for an INA219 not set in RON config")
+            .clone()
+            .into();
+
+        let driver_instance = Ina219::new(
+            ResistanceUnit::milliohms(100.0),
+            CurrentUnit::milliamps(1000.0),
+            target_addr).expect("INA219 driver instantiation error");
+
+        Ok(Self { driver_instance, target_addr })
+    }
+
+    fn process(&mut self, _clock: &RobotClock, msg: &mut Self::Output<'_>) -> CuResult<()> {
+        let dev = &mut self.driver_instance;
+
+        // cheeky
+        let power_reading = dev.power().map_err(|_| -> () {}).unwrap();
+        let current_reading = dev.load_current().map_err(|_| -> () {}).unwrap();
+        let shunt_voltage_reading = dev.shunt_voltage().map_err(|_| -> () {}).unwrap();
+        let bus_voltage_reading = dev.bus_voltage().map_err(|_| -> () {}).unwrap();
+
+        msg.set_payload(
+            Ina219Payload {
+                power           : power_reading.get_val(),
+                load_current    : current_reading.get_val(),
+                shunt_voltage   : shunt_voltage_reading.get_val(),
+                bus_voltage     : bus_voltage_reading.get_val(),
+                target_addr     : self.target_addr
+            }
+        );
+        Ok(())
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    // use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    // #[test]
+    // fn it_works() {
+    //     let result = add(2, 2);
+    //     assert_eq!(result, 4);
+    // }
 }
