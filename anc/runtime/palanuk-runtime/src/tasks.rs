f928d04606a8d5ddd4f8a3654f10152e0e95a0ae
andergisomon <volintine_21001524@utp.edu.my>
2025-11-16 01:49:54 +0800

    add comms deps: zenoh and iceoryx2

--- anc/runtime/palanuk-runtime/src/tasks.rs
+++ anc/runtime/palanuk-runtime/src/tasks.rs
@@ -1,8 +1,51 @@
 use cu29::prelude::*;
-use cu_propulsion::*;
-use cu_cam_pan::*;
-use cu_hcsr04::*;
+use iceoryx2::prelude::*;
+use cu_propulsion::{PropulsionPayload};
+use cu_cam_pan::{CameraPanningPayload};
+use cu_hcsr04::{HcSr04Payload};
 use cu_powermon::*;
 
 pub struct Jogger {}
 pub struct Panner {}
+
+impl Freezable for Jogger {}
+impl Freezable for Panner {}
+
+impl CuTask for Jogger {
+    type Input<'m> = input_msg!('m, HcSr04Payload);
+    type Output<'m> = output_msg!(PropulsionPayload);
+
+    fn new(_config: Option<&ComponentConfig>) -> CuResult<Self>
+    where Self: Sized
+    {
+        Ok(Self {})
+    }
+
+    fn start(&mut self, _clock: &RobotClock) -> CuResult<()> {
+        // use this method to init iox2 sub
+    }
+
+    fn process<'i, 'o>(&mut self, _clock: &RobotClock, input: &Self::Input<'i>, output: &mut Self::Output<'o>,)
+    -> CuResult<()>
+    {
+
+    }
+}
+
+impl CuSrcTask for Panner {
+    type Output<'m> = output_msg!(CameraPanningPayload);
+
+    fn new(_config: Option<&ComponentConfig>) -> CuResult<Self>
+    where Self: Sized
+    {
+        Ok(Self {})
+    }
+
+    fn start(&mut self, _clock: &RobotClock) -> CuResult<()> {
+        // use this method to init iox2 sub
+    }
+
+    fn process(&mut self, clock: &RobotClock, new_msg: &mut Self::Output<'_>) -> CuResult<()> {
+
+    }
+}
