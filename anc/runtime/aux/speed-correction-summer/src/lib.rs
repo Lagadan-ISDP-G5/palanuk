e118451d5e68000a79326fc4c8cd064dd027f381
andergisomon <volintine_21001524@utp.edu.my>
2026-02-22 13:20:12 +0800

    stall recovery when going over bump

--- anc/runtime/aux/speed-correction-summer/src/lib.rs
+++ anc/runtime/aux/speed-correction-summer/src/lib.rs
@@ -2,10 +2,15 @@
 use cu29::prelude::*;
 use cu_propulsion::PropulsionPayload;
 use cu_pid::PIDControlOutputPayload;
+use cu_irencoder::IrEncoderPayload;
 
 pub const R_WIND_COMP_LMTR: f32 = 1.1;
 pub const R_WIND_COMP_RMTR: f32 = 1.0;
 pub const MAX_PID_CORRECTION: f32 = 0.25;
+
+pub const STALL_CMD_THRESHOLD: f32 = 0.15;
+pub const STALL_SPEED_THRESHOLD: f32 = 0.05;
+pub const STALL_BOOST: f32 = 0.3;
 
 pub struct SpeedCorrectionSummer {
     last_output: Option<PropulsionPayload>,
@@ -26,7 +31,7 @@ impl Default for SpeedCorrectionSummer {
 impl Freezable for SpeedCorrectionSummer {}
 
 impl CuTask for SpeedCorrectionSummer {
-    type Input<'m> = input_msg!('m, PIDControlOutputPayload, PIDControlOutputPayload, PropulsionPayload);
+    type Input<'m> = input_msg!('m, PIDControlOutputPayload, PIDControlOutputPayload, PropulsionPayload, IrEncoderPayload);
     type Output<'m> = output_msg!(PropulsionPayload);
     type Resources<'r> = ();
 
@@ -66,6 +71,9 @@ impl CuTask for SpeedCorrectionSummer {
         let lmtr_speed_ctrlr_outpload = input.0.payload();
         let rmtr_speed_ctrlr_outpload = input.1.payload();
         let feedforward = input.2.payload();
+        let encoder = input.3.payload();
+
+        let (lmtr_stall_boost, rmtr_stall_boost) = self.motor_stall_handler(encoder);
 
         if let Some(ff) = feedforward {
             let lmtr_pid = lmtr_speed_ctrlr_outpload.map(|p| p.output).unwrap_or(0.0)
@@ -76,11 +84,8 @@ impl CuTask for SpeedCorrectionSummer {
             let lmtr_ff = ff.left_speed;
             let rmtr_ff = ff.right_speed;
 
-            let lmtr_summed_speed = lmtr_pid + (self.k_ff_lmtr * lmtr_ff);
-            let rmtr_summed_speed = rmtr_pid + (self.k_ff_rmtr * rmtr_ff);
-
-            // eprintln!("PID L={:.4} R={:.4} | FF L={:.4} R={:.4} | OUT L={:.4} R={:.4}",
-                // lmtr_pid, rmtr_pid, lmtr_ff, rmtr_ff, lmtr_summed_speed, rmtr_summed_speed);
+            let lmtr_summed_speed = lmtr_pid + (self.k_ff_lmtr * lmtr_ff) + lmtr_stall_boost;
+            let rmtr_summed_speed = rmtr_pid + (self.k_ff_rmtr * rmtr_ff) + rmtr_stall_boost;
 
             let mut output_msg = ff.clone();
             output_msg.left_speed = (R_WIND_COMP_LMTR * lmtr_summed_speed).clamp(0.0, 1.0);
@@ -95,4 +100,40 @@ impl CuTask for SpeedCorrectionSummer {
         Ok(())
     }
 
+}
+
+// TODO: test this
+// if it doesnt work then we probably need a timer based subroutine that goes like this:
+// stop motor, wait, then go full blast until cu-irencoder registers movement past a certain threshold
+impl SpeedCorrectionSummer {
+    fn motor_stall_handler(&self, encoder: Option<&IrEncoderPayload>) -> (f32, f32) {
+        let last = match self.last_output {
+            Some(ref lo) => lo,
+            None => return (0.0, 0.0),
+        };
+
+        let enc = match encoder {
+            Some(e) => e,
+            None => return (0.0, 0.0),
+        };
+
+        let lmtr_actual = enc.lmtr_normalized_rpm.unwrap_or(0.0);
+        let rmtr_actual = enc.rmtr_normalized_rpm.unwrap_or(0.0);
+
+        let lmtr_boost = if last.left_speed >= STALL_CMD_THRESHOLD && lmtr_actual < STALL_SPEED_THRESHOLD {
+            // eprintln!("STALL L: cmd={:.4} actual={:.4}", last.left_speed, lmtr_actual);
+            STALL_BOOST
+        } else {
+            0.0
+        };
+
+        let rmtr_boost = if last.right_speed >= STALL_CMD_THRESHOLD && rmtr_actual < STALL_SPEED_THRESHOLD {
+            // eprintln!("STALL R: cmd={:.4} actual={:.4}", last.right_speed, rmtr_actual);
+            STALL_BOOST
+        } else {
+            0.0
+        };
+
+        (lmtr_boost, rmtr_boost)
+    }
 }
