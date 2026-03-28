c05bf0874b8ff6143cc859309dcf8c19da6b0764
andergisomon <volintine_21001524@utp.edu.my>
2026-03-28 15:21:45 +0800

    integrated on mac

--- odd/dashboard/src/routes/+layout.svelte
+++ odd/dashboard/src/routes/+layout.svelte
@@ -1,17 +1,5 @@
 <script lang="ts">
-  import { onMount, onDestroy } from 'svelte';
-  // import { connectToZenoh, disconnectFromZenoh } from '../lib/stores/zenohStore';
   import '../app.css';
-
-  onMount(() => {
-    // Connect to Zenoh WebSocket bridge
-    connectToZenoh('ws://localhost:8081/ws');
-  });
-
-  onDestroy(() => {
-    // Cleanup on component destroy
-    disconnectFromZenoh();
-  });
 </script>
 
 <nav class="bg-gray-800 p-4">
